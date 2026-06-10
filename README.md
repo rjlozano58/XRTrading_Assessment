a255a9856951308257fc57f6d15e2ff33e43e333
Rogelio Lozano <rjlozano58@gmail.com>
2026-06-10 14:25:49 -0500

    edited README

--- README.md
+++ README.md
@@ -16,6 +16,13 @@ The application is executed from the command line. It requires three input files
 ```
 python main.py -t TeamMap.csv -p ProductMaster.csv -s Sales.csv --team-report TeamReport.csv --product-report ProductReport.csv
 ```
+**or**
+```
+python main.py -t TeamMap.csv -p ProductMaster.csv -s Sales.csv
+```
+If report file names are not included, the default report names are `TeamReport.csv` and `ProductReport.csv`
+
+
 
 ### Command-Line Arguments
 | Argument | Short | Required | Description |
@@ -30,9 +37,9 @@ python main.py -t TeamMap.csv -p ProductMaster.csv -s Sales.csv --team-report Te
 
 To ensure this application meets **production-ready** standards, it was designed with memory efficiency and scalability in mind:
 
-1. **Streaming Data Processing:** Rather than loading the potentially massive `Sales.csv` file entirely into a dictionary, we read the sales data row-by-row, process the data in each iteration, and immediately discards the row. This ensures that our application uses memory efficiently and performs quicker.
+1. **Streaming Data Processing:** Rather than loading the potentially massive `Sales.csv` file entirely into a dictionary, we read the sales data row-by-row, process the data in each iteration, and immediately discard the row. This ensures that our application uses memory efficiently and performs quicker.
 2. **O(1) Dictionary Lookups:** Loading important data into dictionaries guarantees quick constant-time lookups while iterating through the sales.
-3. **Defensive Programming:** The code includes safety checks to gracefully handle bad data (e.g., verifying that a `team_id` or `product_id` found in the sales logs actually exists in the master reference dictionaries before attempting calculations).
+3. **Fail-Safes with Error Handling:** The code implements `try .. except` blocks to handle unexpected errors, such as missing input files or malformed data row. This allows our application to fail safely, and provides us with useful error messages which can help us fix bugs.
 4. **Argparse Implementation:** Built-in `argparse` is used to handle command-line interfaces robustly, providing automatic type checking, help menus, and fallback default values for optional report outputs.
 
 ## Outputs
--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import json
 
 def team_report(team_dict,output_filename):
 
+    # Headers for ouput TeamReport.csv file
     csv_headers = ["Team","GrossRevenue"]
+
+    # Sort teams by 'gross_revenue' in decreasing order
+    sorted_teams = list(team_dict.values())
+    sorted_teams.sort(key=lambda x: x["gross_revenue"], reverse=True)
 
     try:
 
@@ -13,18 +18,23 @@ def team_report(team_dict,output_filename):
 
             writer.writeheader()
 
-            for team_id, team in team_dict.items():
+            # Iterate through all products, output necessary product details to report csv file
+            for team in sorted_teams:
                 row_to_write = {
                     "Team": team["name"],
                     "GrossRevenue": team["gross_revenue"]
                 }
                 writer.writerow(row_to_write)
+                
     except Exception as e:
         print(f"An error occurred writing '{output_filename}': {e}")
 
 def product_report(product_dict, output_filename):
+
+    # Headers for ouput ProductReport.csv file
     csv_headers = ["Name","GrossRevenue","TotalUnits","DiscountCost"]
 
+    # Sort products by 'gross_revenue' in decreasing order
     sorted_products = list(product_dict.values())
     sorted_products.sort(key=lambda x: x["gross_revenue"], reverse=True)
 
@@ -34,6 +44,7 @@ def product_report(product_dict, output_filename):
 
             writer.writeheader()
 
+            # iterate through all products, output necessary product details to report csv file
             for product in sorted_products:
                 row_to_write = {
                     "Name": product["name"],
@@ -42,13 +53,16 @@ def product_report(product_dict, output_filename):
                     "DiscountCost": product["discount_cost"]
                 }
                 writer.writerow(row_to_write)
+
     except Exception as e:
         print(f"An error occurred writing '{output_filename}': {e}")
 
 
 def main():
+
     parser = argparse.ArgumentParser(description="This application will read from 3 input files and use the data within to produce 2 output files") 
 
+    # Set up arguments
     parser.add_argument("-t", "--team", type=str, help="This should be a CSV of the team data", required=True)
     parser.add_argument("-p", "--product", type=str, help="This should be a CSV of the product master data", required=True)
     parser.add_argument("-s", "--sales", type=str, help="This should be a CSV of the sales data", required=True)
@@ -60,7 +74,7 @@ def main():
     # Save input files name inorder to traverse them
     input_files = [args.team, args.product, args.sales]
 
-    # 
+    # If output filenames are provided, use them. If no filenames provided, default output filenames are use.
     team_out = args.team_report if args.team_report else "TeamReport.csv"
     product_out = args.product_report if args.product_report else "ProductReport.csv"
 
@@ -96,13 +110,13 @@ def main():
                 elif filename == args.sales:
                     for row in reader:
 
-                        # extract sales details in order to calculate gross revenue of each team
+                        # Extract sales details in order to calculate gross revenue of each team
                         product_id = row[1].strip()
                         team_id = row[2].strip()
                         quantity = int(row[3])
                         discount = float(row[4])
 
-                        # extract product details to calculate the gross revenue of each team
+                        # Extract product details to calculate the gross revenue of each team
                         name = product_master_dict[product_id]["name"]
                         price_per_unit = float(product_master_dict[product_id]["price"])
                         lot_size = float(product_master_dict[product_id]["lot_size"])
@@ -110,11 +124,11 @@ def main():
                         gross_revenue_sale = price_per_unit * quantity * lot_size
                         discount_cost = gross_revenue_sale * (discount / 100) 
 
-                        # using sale and product details, append revenue from each sale to gross sale of each team
+                        # Using sale and product details, append revenue from each sale to gross sale of each team
                         if team_id in team_dict:
                             team_dict[team_id]["gross_revenue"] += gross_revenue_sale
 
-                            # using sale and product details, append the revenue from this product's sale to it's gross revenue in products dictionary
+                            # Using sale and product details, append the revenue from this product's sale to it's gross revenue in products dictionary
                             product_master_dict[product_id]["gross_revenue"] += gross_revenue_sale
                             product_master_dict[product_id]["total_units"] += quantity * lot_size
                             product_master_dict[product_id]["discount_cost"] += discount_cost
@@ -127,11 +141,6 @@ def main():
         except Exception as e:
             print(f"An unexpected error occurred while reading '{filename}': {e}")
             sys.exit(1)
-
-    # for debugging
-    # print(team_dict)
-    # print()
-    # print(product_master_dict)
 
     # Create team report and product report csv files
     team_report(team_dict,team_out)
